ff36fb546b5c85ae3ebbc90561efd4dadafa48a9
John Dzak <john.dzak@gmail.com>
2011-12-07 21:59:00 -0600

    Remove logging statement.

--- NCSMobile/NUScrollView.m
+++ NCSMobile/NUScrollView.m
@@ -14,7 +14,6 @@
     CGRect r = subview.frame;
     CGFloat newX = MAX(self.contentSize.width, r.origin.x + r.size.width);
     CGFloat newY = MAX(self.contentSize.height, r.origin.y + r.size.height);
-//    NSLog(@"newX [%@ : %@]", newX, newY);
     self.contentSize = CGSizeMake(newX, newY);
 }
                        
