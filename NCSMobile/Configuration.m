6b6b2652affc9b9ed88617438e502c24295227bc
John Dzak <john.dzak@gmail.com>
2012-01-16 14:18:36 -0600

    Add configuration class.

---
+++ NCSMobile/Configuration.m
@@ -0,0 +1,13 @@
+//
+//  Configuration.m
+//  NCSMobile
+//
+//  Created by John Dzak on 1/16/12.
+//  Copyright (c) 2012 Northwestern University. All rights reserved.
+//
+
+#import "Configuration.h"
+
+@implementation Configuration
+
+@end
